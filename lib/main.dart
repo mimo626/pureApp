fae8d3688793eab309f20723cfae9d6833c4f1fe
mimo626 <yessuk5499@gmail.com>
2024-05-13 22:34:47 +0900

    [DB] Firebase와 연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter03_mingflix/provider/navigation_index_provider.dart';
 import 'package:flutter03_mingflix/screen/main_screen.dart';
 import 'package:flutter03_mingflix/widget/main_bottom_navigation_bar.dart';
 import 'package:provider/provider.dart';
 
-void main(){
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(PureApp());
 }
 
